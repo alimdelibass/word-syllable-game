7c919ecf95d29fa0cc7968b7cde416d548ec3cec
Alim delibas <alim.delibass@gmail.com>
2023-12-30 22:37:45 +0300

    Initial Commit

---
+++ Peroject8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Peroject8
+//
+//  Created by alim on 30.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
